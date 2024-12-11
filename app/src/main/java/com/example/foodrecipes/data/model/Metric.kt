22a4bdcc34bb6db484b2fbf084dfd129b64c80f6
denizsimsek <ddenizsimsekk@gmail.com>
2024-12-12 00:15:31 +0300

    Parcelable Annotation Removed.

--- app/src/main/java/com/example/foodrecipes/data/model/Metric.kt
+++ app/src/main/java/com/example/foodrecipes/data/model/Metric.kt
@@ -1,11 +1,9 @@
 package com.example.foodrecipes.data.model
 
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class Metric(
     val amount: Double,
     val unitLong: String,
     val unitShort: String
-):Parcelable
+)
